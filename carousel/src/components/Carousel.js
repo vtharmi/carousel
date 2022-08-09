347b3bb17c87bf44c43b398ac6192ab6a2e8de2f
tharmi.vijayakumaran <tharmi.vijayakumaran@itelasoft.com>
2022-08-09 22:51:24 +0530

    added loading and removed dummy data

--- carousel/src/components/Carousel.js
+++ carousel/src/components/Carousel.js
@@ -4,7 +4,8 @@ import "../styles/styles.css";
 
 function Carousel({slides, infinite}){
   const [state, setState] = useState(0);
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true)
 
   const prevSlideHandler = () => {
     let newPosition = state;
@@ -32,42 +33,43 @@ const nextClickHandler = () => {
   nextSlideHandler();
 }
  
-    const imgUrl = [
-      "https://wallpaperbro.com/img/294233.jpg",
-      "https://cdn.pixabay.com/photo/2015/07/03/15/46/wallpaper-830417_960_720.jpg",
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaSqb8L4hb9zljCAlV5WvTR42NuiTki0N9TRu7_nae&s",
-      "https://images.unsplash.com/photo-1619566636858-adf3ef46400b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&w=1000&q=80",
-      "https://images.pexels.com/photos/1132047/pexels-photo-1132047.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-    ];
+    // const imgUrl = [
+    //   "https://wallpaperbro.com/img/294233.jpg",
+    //   "https://cdn.pixabay.com/photo/2015/07/03/15/46/wallpaper-830417_960_720.jpg",
+    //   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaSqb8L4hb9zljCAlV5WvTR42NuiTki0N9TRu7_nae&s",
+    //   "https://images.unsplash.com/photo-1619566636858-adf3ef46400b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&w=1000&q=80",
+    //   "https://images.pexels.com/photos/1132047/pexels-photo-1132047.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+    // ];
 
     useEffect(() => {
-     GetCarouselImages(`?slides=${slides}`).then(res => {
-      setData(res);
-      console.log("result", res)
-     })
-    }, [slides])
+      if(loading) {
+        GetCarouselImages(`?slides=${slides}`).then(res => {
+          setData(res);
+          console.log("result", res);
+         
+         })
+      }
+    
+     setLoading(false)
+    }, [slides, loading])
 
 
     return (
      <>
-     {
-      // data.length > 0 ?? (
-        <div className="slide-container">
-        <div className="mySlides fade">
-          <div className="numbertext">{state} / {slides}</div>
-        <div className="LeftArrow" onClick={prevClickHandler}>❰</div>
-          <img className="image" src={imgUrl[state]} alt="slide"></img>
-          {/* <div className="CarouselItem">
-        {imgUrl.map(img => {
-            <img  src={img} alt="slide" ></img>
-        })}  
-                </div> */}
-            
-        <div className="RightArrow" onClick={nextClickHandler}>❱</div>
-        </div>
-      
+    {loading && <p>loading</p>}
+    {
+     !loading &&  (data.length > 0 && (
+      <div className="slide-container">
+      <div className="mySlides fade">
+        <div className="numbertext">{state} / {slides}</div>
+      <div className="LeftArrow" onClick={prevClickHandler}>❰</div>
+        <img className="image" src={data[state].image} alt="slide" />
+   
+      <div className="RightArrow" onClick={nextClickHandler}>❱</div>
       </div>
-      // )
+    
+    </div>
+    ))
      }
      </>
     );
